b9f4fda0770cc9f7f0064605ee3b40e1d0f7eb55
TinYu2020 <tyu@palo-it.com>
2023-02-25 20:56:39 +0800

    change one instance to cluster of web servers: create a launch configuration, ASG, ALB, and a security group

--- main.tf
+++ main.tf
@@ -2,21 +2,20 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resource "aws_instance" "ec2_frontend" {
-    ami = "ami-0fb653ca2d3203ac1"
+resource "aws_launch_configuration" "ec2_frontend" {
+    image_id = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+    security_groups = [aws_security_group.instance.id]
 
     user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World!" > index.html
+              echo "Hello, World!!" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
-    user_data_replace_on_change = true
-
-    tags = {
-        Name = "terraform-example"
+    # required for ASG
+    lifecycle {
+        create_before_destroy = true
     }
 }
 
@@ -31,13 +30,120 @@ resource "aws_security_group" "instance" {
     }
 }
 
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.ec2_frontend.name
+    vpc_zone_identifier = data.aws_subnets.default.ids
+    
+    target_group_arns = [aws_lb_target_group.asg.arn]
+    health_check_type = "ELB"
+
+    min_size = 2
+    max_size = 4
+    
+    tag {
+        key = "Name"
+        value = "terraform-asg-example"
+        propagate_at_launch = true
+    }
+}
+
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnets" "default" {
+    filter {
+        name = "vpc-id"
+        values = [data.aws_vpc.default.id]
+    }
+}
+
+resource "aws_lb" "example" {
+    name = "terraform-asg-example"
+    load_balancer_type = "application"
+    subnets = data.aws_subnets.default.ids
+    security_groups = [aws_security_group.alb.id]
+}
+
+# config for application load balancer
+resource "aws_lb_listener" "http" {
+    load_balancer_arn = aws_lb.example.arn
+    port = 80
+    protocol = "HTTP"
+
+    # return 404 page by default
+    default_action {
+        type = "fixed-response"
+
+        fixed_response {
+            content_type = "text/plain"
+            message_body = "404: page not found"
+            status_code = 404
+        }
+    }
+}
+
+# security group for application load balancer
+resource "aws_security_group" "alb" {
+    name = "terraform-example-alb"
+
+    # allow inbound HTTP requests
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # allow all outbound requests
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_lb_target_group" "asg" {
+    name = "terraform-asg-example"
+    port = var.server_port
+    protocol = "HTTP"
+    vpc_id = data.aws_vpc.default.id
+
+    health_check {
+        path = "/"
+        protocol = "HTTP"
+        matcher = "200"
+        interval = 15
+        timeout = 3
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+    }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+    listener_arn = aws_lb_listener.http.arn
+    priority = 100
+
+    condition {
+        path_pattern {
+            values = ["*"]
+        }
+    }
+
+    action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.asg.arn
+    }
+}
+
 variable "server_port" {
     description = "The port the server will use for HTTP requests" 
     type = number
     default = 8080
 }
 
-output "public_ip" {
-    value = aws_instance.ec2_frontend.public_ip
-    description = "The public IP address of the web server"
+output "alb_dns_name" {
+    value = aws_lb.example.dns_name
+    description = "The domain name of the load balancer"
 }
