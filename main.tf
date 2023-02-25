c445f596d6c50445c4a220b5044d94cff1e775bf
TinYu2020 <tyu@palo-it.com>
2023-02-25 19:21:56 +0800

    add security group to the instance

--- main.tf
+++ main.tf
@@ -1,12 +1,43 @@
 provider "aws" {
-    region = "ap-east-1"
+    region = "us-east-2"
 }
 
 resource "aws_instance" "ec2_frontend" {
-    ami = "ami-008834de6141fb49b"
-    instance_type = "t4g.nano"
+    ami = "ami-0fb653ca2d3203ac1"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World!" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+    user_data_replace_on_change = true
 
     tags = {
         Name = "terraform-example"
     }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = var.server_port
+        to_port = var.server_port
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+variable "server_port" {
+    description = "The port the server will use for HTTP requests" 
+    type = number
+    default = 8080
+}
+
+output "public_ip" {
+    value = aws_instance.ec2_frontend.public_ip
+    description = "The public IP address of the web server"
 }
