35636fdfc1ff07a8419e15a8815c21f353b89bb4
TinYu2020 <tyu@palo-it.com>
2023-03-01 18:12:05 +0800

    correct testing string

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World testing build can fail!';
+    return 'Hello World!';
   }
 }
